6d705a26e6c94678731624730e8de0288194a778
Jordi Boggiano <j.boggiano@seld.be>
2011-02-09 17:53:36 +0100

    Removed now-useless methods

--- DoctrinePHPCRBundle.php
+++ DoctrinePHPCRBundle.php
@@ -8,19 +8,4 @@ use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class DoctrinePHPCRBundle extends Bundle
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function getNamespace()
-    {
-        return __NAMESPACE__;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getPath()
-    {
-        return strtr(__DIR__, '\\', '/');
-    }
 }
